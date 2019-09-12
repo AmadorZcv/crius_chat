8e25fd72d4f1fd97a0955d4f5cf15495ee95b170
AmadorZcv <lucasamadorzcv@gmail.com>
2019-09-11 23:36:18 -0300

    Modulo de autenticação Dependencia de autenticação

--- lib/crius_chat/auth.ex
+++ lib/crius_chat/auth.ex
@@ -7,6 +7,41 @@ defmodule CriusChat.Auth do
   alias CriusChat.Repo
 
   alias CriusChat.Auth.User
+  alias CriusChat.Auth.AuthToken
+  alias CriusChat.Services.Authenticator
+
+  def validate_user_token(id, token) do
+    from(
+      u in User,
+      join: t in AuthToken,
+      where: u.id == ^id and t.token == ^token and t.revoked == false
+    )
+    |> Repo.one()
+  end
+
+  def sign_in(login, password) do
+    case Bcrypt.check_pass(Repo.get_by(User, login: login), password) do
+      {:ok, user} ->
+        token = Authenticator.generate_token(user.id)
+        Repo.insert(Ecto.build_assoc(user, :auth_tokens, %{token: token}))
+
+      err ->
+        err
+    end
+  end
+
+  def sign_out(conn) do
+    case Authenticator.get_auth_token(conn) do
+      {:ok, token, _id} ->
+        case Repo.get_by(AuthToken, %{token: token}) do
+          nil -> {:error, :not_found}
+          auth_token -> Repo.delete(auth_token)
+        end
+
+      error ->
+        error
+    end
+  end
 
   @doc """
   Returns the list of users.
---
+++ lib/crius_chat/services/authenticator.ex
@@ -0,0 +1,40 @@
+defmodule CriusChat.Services.Authenticator do
+  @seed "VmjTRUwb4z87PKVgmtsJeof1HKufbyg"
+  @secret "Y5Z3oL0mgkx9/gKDnUodLmkvdFDzgGpnh5ZWyoxj02M2NHALlrattIEJPWjTn8f5"
+
+  def generate_token(id) do
+    Phoenix.Token.sign(@secret, @seed, id, max_age: 86400)
+  end
+
+  def verify_token(token) do
+    case Phoenix.Token.verify(@secret, @seed, token) do
+      # 0 - User, 1 - Admin
+      {:ok, id} -> {:ok, token, id}
+      error -> error
+    end
+  end
+
+  def get_auth_token(conn) do
+    case extract_token(conn) do
+      {:ok, token} -> verify_token(token)
+      error -> error
+    end
+  end
+
+  # extrai o token do conn
+  defp extract_token(conn) do
+    case Plug.Conn.get_req_header(conn, "authorization") do
+      [auth_header] ->
+        {:ok, String.trim(auth_header)}
+
+      _ ->
+        case Plug.Conn.get_session(conn, :token) do
+          nil ->
+            {:error, :missing_auth_header}
+
+          token ->
+            {:ok, token.token}
+        end
+    end
+  end
+end
